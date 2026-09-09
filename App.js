46af935fa57958f37d4a5a232efa376f539bf4f8
akshitagarg275 <akshitagarg275@gmail.com>
2026-09-09 09:27:03 +0530

    jsx is added

--- App.js
+++ App.js
@@ -1,2 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+const heading = <h1>H1 heading using JSX</h1>
+
+const HeadingComponent = () => {
+    return (<h2>Heading JS component</h2>);
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
